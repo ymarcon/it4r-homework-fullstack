07232796b13eec82f1fbf5b4242699259b3dbfcf
David Tang <david.tang@epfl.ch>
2023-02-10 10:29:37 +0100

    feat: add api get canton

--- backend/homework_fullstack/main.py
+++ backend/homework_fullstack/main.py
@@ -1,4 +1,5 @@
-from typing import Union
+from csv import DictReader
+from typing import Dict, List, Union
 
 from fastapi import FastAPI
 
@@ -13,3 +14,13 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/cantons")
+async def get_cantons():
+    cantons: List[Dict[str, str]] = []
+    with open("../dataset/canton.csv") as file:
+        reader = DictReader(file)
+        for row in reader:
+            cantons.append(row)
+    return cantons
